99de3249284622daaf4afbb03bd370c3bae9c98b
Enzo <evaghetti@protonmail.com>
2019-05-24 16:05:49 -0300

    Drift dos broder

--- Asteroids/Nave.cs
+++ Asteroids/Nave.cs
@@ -10,6 +10,7 @@ namespace Asteroids
 
         private readonly Texture textura;
         private readonly Sprite sprite;
+        private Vector2f velocidade;
 
         private float angulo, aceleration;
 
@@ -17,9 +18,10 @@ namespace Asteroids
             //Mudar dps quando eu descobrir onde que fica os arquivos do projeto ._.
             textura = new Texture("C:\\Users\\Enzo\\Programas\\Asteroids\\Asteroids\\Asteroids\\Imagens\\player.png");
             sprite = new Sprite(textura);
+            velocidade = new Vector2f(0f, 0f);
 
             angulo = 0f;
-            aceleration = 150f;
+            aceleration = 5f;
 
             sprite.Scale *= .15f;
             sprite.Origin = new Vector2f(343f, 383f) * .5f;
@@ -29,12 +31,16 @@ namespace Asteroids
             int dirThrottle = Convert.ToInt32(Keyboard.IsKeyPressed(Keyboard.Key.Up)) - Convert.ToInt32(Keyboard.IsKeyPressed(Keyboard.Key.Down));
             int dirTurn = Convert.ToInt32(Keyboard.IsKeyPressed(Keyboard.Key.Right)) - Convert.ToInt32(Keyboard.IsKeyPressed(Keyboard.Key.Left));
 
-            angulo += (100f * deltaTime) * dirTurn;
+            angulo += (500f * deltaTime) * dirTurn;
             if (dirThrottle != 0) {
                 float anguloRadiano = (angulo - 90f) * 0.0174533f;
-                position.X += MathF.Cos(anguloRadiano) * dirThrottle * aceleration * deltaTime;
-                position.Y += MathF.Sin(anguloRadiano) * dirThrottle * aceleration * deltaTime;
+                velocidade.X += MathF.Cos(anguloRadiano) * dirThrottle * aceleration * deltaTime;
+                velocidade.Y += MathF.Sin(anguloRadiano) * dirThrottle * aceleration * deltaTime;
             }
+            else
+                velocidade *= 0.99f;
+
+            position += velocidade;
 
             sprite.Position = position;
             sprite.Rotation = angulo;
