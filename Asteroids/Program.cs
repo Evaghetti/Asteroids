7a30b896f6ed6516ff01420b831078bddfb1b80d
Enzo <evaghetti@protonmail.com>
2019-05-24 13:51:35 -0300

    Simples teste pra ver se tá tudo funcionando e quais as diferenças da API do C# pra do C++

---
+++ Asteroids/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using SFML.Graphics;
+
+namespace Asteroids {
+    class Program {
+        static void Main(string[] args) {
+            RenderWindow window = new RenderWindow(new SFML.Window.VideoMode(640, 480), "Asteroids C#");
+            window.Closed += (_, __) => window.Close();
+
+            while (window.IsOpen) {
+                window.DispatchEvents();
+
+                window.Clear(Color.Red);
+                window.Display();
+            }
+        }
+    }
+}
