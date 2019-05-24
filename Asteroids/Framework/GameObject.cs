69e0b2626af70c60fbda1a83a34ff4f2ab2e6a79
Enzo <evaghetti@protonmail.com>
2019-05-24 14:44:21 -0300

    Adcionada classe pra GameObjects

---
+++ Asteroids/Framework/GameObject.cs
@@ -0,0 +1,21 @@
+﻿using SFML.System;
+using SFML.Graphics;
+
+namespace Asteroids.Framework {
+    abstract class GameObject {
+        Vector2f position, tamanho;
+
+        private GameObject(Vector2f position, Vector2f tamanho) {
+            this.position = position;
+            this.tamanho = tamanho;
+        }
+
+        public bool ColidiuCom(GameObject outro) {
+            FloatRect minhaCaixa = new FloatRect(position, tamanho), outraCaixa = new FloatRect(outro.position, outro.tamanho);
+            return minhaCaixa.Intersects(outraCaixa);
+        }
+
+        public abstract float Update(float deltaTime);
+        public abstract void Draw(RenderTarget target);
+    }
+}
