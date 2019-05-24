77177ffdbb5119112acfa1b9fcf2e1e57faa1cdf
Enzo <evaghetti@protonmail.com>
2019-05-24 16:12:23 -0300

    Bye Bye fundo vermelho

--- Asteroids/PlayState.cs
+++ Asteroids/PlayState.cs
@@ -6,8 +6,12 @@ namespace Asteroids
     class PlayState : GameState {
 
         private Nave navezinha;
+        private Texture texturaFundo;
+        private Sprite fundo;
         public PlayState(RenderWindow window) : base(window) {
             navezinha = new Nave(new SFML.System.Vector2f(50f, 50f));
+            texturaFundo = new Texture("C:\\Users\\Enzo\\Programas\\Asteroids\\Asteroids\\Asteroids\\Imagens\\space-pure.jpg");
+            fundo = new Sprite(texturaFundo);
         }
 
         public override void Update() {
@@ -17,7 +21,8 @@ namespace Asteroids
         }
 
         public override void Draw() {
-            window.Clear(Color.Red);
+            window.Clear();
+            window.Draw(fundo);
             navezinha.Draw(window);
             window.Display();
         }
