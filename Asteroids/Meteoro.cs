834c77047dd44d558ab8738c536f2cc9d677a8dc
rafaelanacleto <rafael.ferreira.anacleto@gmail.com>
2019-10-16 10:23:36 -0300

    Spawn e movimento dos meteoros parece estar certo, só precisa de uns ajustes

--- Asteroids/Meteoro.cs
+++ Asteroids/Meteoro.cs
@@ -7,12 +7,22 @@ using System.Text;
 
 namespace Asteroids {
     class Meteoro : GameObject {
+        private static readonly float velocidade = 5f;
+        private readonly float angulo;
+
         private Animation animations;
         private Sprite sprite;
 
-        public Meteoro() : base(new Vector2f(0f, 0f), new Vector2f(50f, 50f)) {
+        public Meteoro(float angulo) : base(new Vector2f(), new Vector2f(50f, 50f)) {
+            this.angulo = angulo;
+            this.position = new Vector2f(320f - MathF.Cos(AnguloRad) * 320f, 240f - MathF.Sin(AnguloRad) * 240f);
+
+            Console.WriteLine($"Meteoro: x{{{position.X}}}, y {{{position.Y}}} angulo {{{angulo}}}");
+
             sprite = new Sprite();
             sprite.Origin = new Vector2f(.5f, .5f);
+            sprite.Rotation = angulo;
+            sprite.Scale = new Vector2f(0.25f, 0.25f);
 
             animations = new Animation("Imagens/meteoros.png", 256, 256, 8, 8);
             animations.AplicarTextura(ref sprite);
@@ -28,7 +38,12 @@ namespace Asteroids {
         public override void Update(float deltaTime) {
             animations.Update(deltaTime);
 
+            position.X += MathF.Cos(AnguloRad) * velocidade;
+            position.Y += MathF.Sin(AnguloRad) * velocidade;
+
             sprite.Position = position;
         }
+
+        public float AnguloRad { get => (angulo - 90f) * 0.0174533f; }
     }
 }
--- Asteroids/PlayState.cs
+++ Asteroids/PlayState.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using SFML.Graphics;
+using SFML.System;
 
 namespace Asteroids
 {
@@ -9,27 +10,42 @@ namespace Asteroids
         private Nave navezinha;
         private Texture texturaFundo;
         private Sprite fundo;
-        private Meteoro meteoro;
 
+        private List<Meteoro> meteoros;
         private List<Framework.GameObject> projeteis;
 
+        private float tempoSpawnMeteoro = 0f;
+        private readonly float limiteTempoSpawnMeteoro = 1f;
+
+        private Random random;
+
         public PlayState(RenderWindow window) : base(window) {
+            meteoros = new List<Meteoro>();
             projeteis = new List<Framework.GameObject>();
             navezinha = new Nave(new SFML.System.Vector2f(50f, 50f));
             texturaFundo = new Texture("Imagens/space-pure.jpg");
+            random = new Random();
             fundo = new Sprite(texturaFundo);
-
-            meteoro = new Meteoro();
         }
 
         public override void Update() {
             float deltaTime = GetDeltaTime();
 
-            navezinha.Update(deltaTime);
-            meteoro.Update(deltaTime);
+            tempoSpawnMeteoro += deltaTime;
+            if (tempoSpawnMeteoro >= limiteTempoSpawnMeteoro) {
+                float anguloMovimento = (float)random.NextDouble() * 360f;
 
+                meteoros.Add(new Meteoro(anguloMovimento));
+
+                tempoSpawnMeteoro = 0f;
+            }
+            navezinha.Update(deltaTime);
+            
             if (navezinha.Atirou)
                 projeteis.Add(new Disparo(navezinha.Position, .05f, navezinha.AnguloRad));
+
+            for (int i = 0; i < meteoros.Count; i++)
+                meteoros[i].Update(deltaTime);
 
             for (int i = 0; i < projeteis.Count; i++) {
                 projeteis[i].Update(deltaTime);
@@ -47,7 +63,8 @@ namespace Asteroids
             navezinha.Draw(window);
             for (int i = 0; i < projeteis.Count; i++)
                 projeteis[i].Draw(window);
-            meteoro.Draw(window);
+            for (int i = 0; i < meteoros.Count; i++)
+                meteoros[i].Draw(window);
 
             window.Display();
         }
